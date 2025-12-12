fcbb57a2bf0f50aeba4d9103fdd9ba4e7ad20c71
Ash-Har-Ali <20gcs53@meaec.edu.in>
2025-12-12 16:38:03 +0530

    Benefits section

--- app/components/ProgramBenefits.tsx
+++ app/components/ProgramBenefits.tsx
@@ -5,43 +5,43 @@ import Image from "next/image";
 const items = [
   {
     title: "Experiential & Activity-Based Learning",
-    img: "/images/highlight1.jpg",
+    img: "/Assets/images/benefits1.png",
   },
   {
     title: "Stress Management And Critical Thinking",
-    img: "/images/highlight2.jpg",
+    img: "/Assets/images/benefits2.png",
   },
   {
     title: "Outdoor Challenges & Team Tasks",
-    img: "/images/highlight3.jpg",
+    img: "/Assets/images/benefits3.png",
   },
   {
     title: "Leadership & Confidence Building",
-    img: "/images/highlight4.jpg",
+    img: "/Assets/images/benefits4.png",
   },
   {
     title: "Mentorship & Emotional Growth",
-    img: "/images/highlight5.jpg",
+    img: "/Assets/images/benefits5.png",
   },
 ];
 
 export default function HighlightsSection() {
   return (
-    <section className="w-full bg-white py-20">
+    <section className="w-full bg-white py-20 md:pt-25">
       <div className="mx-auto flex md:flex-row flex-col max-w-6xl items-center justify-between gap-6 px-6">
         {items.map((item, index) => {
           const staggerClass =
             index === 1 || index === 3
-              ? "md:-translate-y-6"
+              ? "md:-translate-y-10"
               : "md:translate-y-0";
 
           return (
             <div
               key={item.title}
-              className={`flex w-[250px] flex-col overflow-hidden rounded-3xl bg-white shadow-md transition-transform ${staggerClass}`}
+              className={`flex w-[300px] flex-col overflow-hidden rounded-3xl bg-white shadow-md transition-transform ${staggerClass}`}
             >
               {/* fixed image height */}
-              <div className="h-60 w-full overflow-hidden">
+              <div className="md:h-65 h-70 w-full overflow-hidden">
                 <Image
                   src={item.img}
                   alt={item.title}
@@ -52,8 +52,8 @@ export default function HighlightsSection() {
               </div>
 
               {/* fixed purple block height */}
-              <div className="flex h-20 items-center justify-center bg-[#7F3EDF] px-3 text-center">
-                <p className="text-[12px] font-semibold leading-snug text-white">
+              <div className="flex md:h-35 py-6 items-center justify-center bg-[#562190] px-8 text-center">
+                <p className="text- font-semibold leading-snug text-white ">
                   {item.title}
                 </p>
               </div>

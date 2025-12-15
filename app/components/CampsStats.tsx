e63fe345da8bb195b9fc8e88808950bd1bfb2899
Ash-Har-Ali <20gcs53@meaec.edu.in>
2025-12-15 11:56:14 +0530

    edits - stats

--- app/components/CampsStats.tsx
+++ app/components/CampsStats.tsx
@@ -8,31 +8,31 @@ export default function CampsStats() {
         {/* Item 1 */}
         <div className="relative flex h-20 w-full items-center justify-center md:w-1/3">
           {/* Big number in background */}
-          <h2 className="pointer-events-none select-none text-7xl font-black text-[#00000080] leading-none tracking-wider">
+          <h2 className="pointer-events-none select-none text-8xl font-black text-[#00000080] leading-none tracking-wider">
             17
           </h2>
           {/* Label on top */}
-          <span className="absolute text-sm font-semibold text-white">
+          <span className="absolute text- font-semibold text-white">
             Camps in India
           </span>
         </div>
 
         {/* Item 2 */}
         <div className="relative flex h-20 w-full items-center justify-center md:w-1/3">
-          <h2 className="pointer-events-none select-none text-7xl font-black text-[#00000080] leading-none tracking-wider">
+          <h2 className="pointer-events-none select-none text-8xl font-black text-[#00000080] leading-none tracking-wider">
             18
           </h2>
-          <span className="absolute text-sm font-semibold text-white">
+          <span className="absolute text- font-semibold text-white">
             Camps in UAE
           </span>
         </div>
 
         {/* Item 3 */}
         <div className="relative flex h-20 w-full items-center justify-center md:w-1/3">
-          <h2 className="pointer-events-none select-none text-7xl font-black text-[#00000080] leading-none tracking-wider">
+          <h2 className="pointer-events-none select-none text-8xl font-black text-[#00000080] leading-none tracking-wider">
             500+
           </h2>
-          <span className="absolute text-sm font-semibold text-white">
+          <span className="absolute text- font-semibold text-white">
             Students
           </span>
         </div>
